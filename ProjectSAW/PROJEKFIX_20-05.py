bebf533860be1da7421e4d7817cb74ed882f4325
Anubluskrin <anri0840@gmail.com>
2026-05-26 12:41:15 +0700

    Remove warnings filter from the script
    Removed warnings filter to allow warning messages.

--- ProjectSAW/PROJEKFIX_20-05.py
+++ ProjectSAW/PROJEKFIX_20-05.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings("ignore")
 
 #  PAGE CONFIG
 st.set_page_config(
